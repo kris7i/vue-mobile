97bf4c52eb85b50d910b2002c91297bfade5f4eb
chengyang <chengyang@chengyang.com>
2020-04-13 10:43:13 +0800

    fix

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -19,7 +19,7 @@ let cancelToken = axios.CancelToken;
 let removePending = (config) => {
   for (let p in pending) {
     if (pending[p].u === config.url + '&' + config.method) {
-      //当当前请求在数组中存在时执行函数体
+      //当前请求在数组中存在时执行函数体
       pending[p].f();//执行取消操作
       pending.splice(p, 1); //把这条记录从数组中移除
     }
